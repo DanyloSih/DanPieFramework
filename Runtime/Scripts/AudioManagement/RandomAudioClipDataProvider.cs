a50f784f2c90b508dc131e1b5e3cf2ceeff98cfb
Danil Sigaev <danil.sigaev@qplaze.com>
2022-12-09 20:56:08 +0200

    AudioManagement | Add RandomAudioClipProvider component.

---
+++ Runtime/Scripts/AudioManagement/RandomAudioClipDataProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using DanPie.Framework.Randomnicity;
+using UnityEngine;
+
+namespace DanPie.Framework.AudioManagement
+{
+    [CreateAssetMenu(fileName = "RandomClipProvider", menuName = "AudioManagement/new RandomClipProvider")]
+    public class RandomAudioClipDataProvider : AudioClipDataProvider
+    {
+        [Serializable]
+        private class SelectableAudioClipData : IRandomSelectableItem
+        {
+            [field: SerializeField] public int SelectionChance { get; }
+            [field: SerializeField] public AudioClipData ClipData { get; }
+        }
+
+        [SerializeField] private List<SelectableAudioClipData> _clips;
+
+        private RandomItemSelector<SelectableAudioClipData> _selector;
+
+        public override AudioClipData GetClipData()
+        {
+            return _selector.GetRandomItem().ClipData;
+        }
+
+        protected void Awake()
+        {
+            _selector = new RandomItemSelector<SelectableAudioClipData>(_clips);
+        }
+    }
+}
