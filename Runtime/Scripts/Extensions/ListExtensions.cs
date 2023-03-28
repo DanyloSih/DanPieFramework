48375b4ae8f4e87ac0cff75ba8d2a90f2fc5c503
DanyloSih <geratosfen@gmail.com>
2023-03-28 22:20:37 +0300

    Create List Extensions.

---
+++ Runtime/Scripts/Extensions/ListExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace DanPie.Framework.Extensions
+{
+    public static class ListExtension
+    {
+        public static T PopAt<T>(this List<T> list, int index)
+        {
+            T r = list[index];
+            list.RemoveAt(index);
+            return r;
+        }
+    } 
+}
