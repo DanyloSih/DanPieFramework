9fe8c5d5702ea9c3fa3db5e849c6f9d928237c38
DanyloSih <geratosfen@gmail.com>
2023-04-06 11:48:55 +0300

    Created FieldDataException.

---
+++ Runtime/Scripts/Exceptions/FieldDataException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DanPie.Framework.Exceptions
+{
+    public class FieldDataException : Exception
+    {
+        public FieldDataException(string fieldClassName, string fieldName, string message) 
+            : base($"There is a problem with the data in field \"{fieldName}\" of class \"{fieldClassName}\"!"
+                  + message)
+        {
+        }
+    }
+}
